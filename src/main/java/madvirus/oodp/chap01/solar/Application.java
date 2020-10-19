5ccb5390b02f9ee5cac65333c4cef02d784d1dff
blossun <dnsngu@gmail.com>
2020-10-19 15:55:59 +0900

    [Feat] : 메뉴 3 추가
    1. 메뉴3 화면 처리 위한 Menu3ScreenUI 클래스
    2. Application 클래스에 메뉴3 관련 코드 추가
    
    ### 얻을 수 있는 장점
    1. 새로운 메뉴 추가 시, 버튼 처리 코드가 영향을 받지 않음
    2. 한 메뉴 관련 코드가 한 개의 클래스로 모여서 코드 분석/수정이 용이함
    3. 서로 다른 메뉴에 대한 처리 코드가 섞여 있지 않아 수정이 용이함
    ⇒ 요구 사항이 바뀔 때, 그 변화를 좀 더 수월하게 적용할 수 있다.
    객체 지향 기법을 적용하면 소프트웨어를 더 쉽게 변경할 수 있는 유연함(flexibility)을 얻을 수 있게 되고, 이는 곧 요구 사항의 변화를 더 빠르게 수용할 수 있다는 것을 뜻한다.
    
    issue #1

--- src/main/java/madvirus/oodp/chap01/solar/Application.java
+++ src/main/java/madvirus/oodp/chap01/solar/Application.java
@@ -4,12 +4,12 @@ import madvirus.oodp.chap01.Button;
 import madvirus.oodp.chap01.Component;
 import madvirus.oodp.chap01.Menu;
 import madvirus.oodp.chap01.OnClickListener;
-import madvirus.oodp.chap01.after2.Menu3ScreenUI;
 
 public class Application { //Application이 OnClickListener을 구현하지 않음
 
     private Menu menu1 = new Menu("menu1");
     private Menu menu2 = new Menu("menu2");
+    private Menu menu3 = new Menu("menu3");
     private Button button1 = new Button("button1");
     private Button button2 = new Button("button2");
 
@@ -18,6 +18,7 @@ public class Application { //Application이 OnClickListener을 구현하지 않�
     public Application() {
         menu1.setOnClickListener(menuListener);
         menu2.setOnClickListener(menuListener);
+        menu3.setOnClickListener(menuListener);
         button1.setOnClickListener(buttonListener);
         button2.setOnClickListener(buttonListener);
     }
@@ -28,6 +29,8 @@ public class Application { //Application이 OnClickListener을 구현하지 않�
                 currentScreen = new Menu1ScreenUI();
             } else if (eventSource.getId().equals("menu2")) {
                 currentScreen = new Menu2ScreenUI();
+            } else if (eventSource.getId().equals("menu3")) {
+                currentScreen = new Menu3ScreenUI();
             }
             currentScreen.show();
         }
