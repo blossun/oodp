c102659c2b3cd726799c5b82bfc0c60a9b6a0a8a
blossun <dnsngu@gmail.com>
2020-10-19 14:27:31 +0900

    [Refactor] : 메뉴1과 메뉴2를 위한 ScreenUI 구현
    메뉴 별로 실제 동작 코드가 다르므로 각 메뉴 별로 ScreenUI 인터페이스를 구현한 클래스를 작성
    
    issue #1

---
+++ src/main/java/madvirus/oodp/chap01/solar/Menu1ScreenUI.java
@@ -0,0 +1,13 @@
+package madvirus.oodp.chap01.solar;
+
+public class Menu1ScreenUI implements ScreenUI {
+    @Override
+    public void show() {
+        System.out.println("메뉴 1 화면으로 전환");
+    }
+
+    @Override
+    public void handleButton1Click() {
+        System.out.println("메뉴1 화면의 버튼1 처리");
+    }
+}
